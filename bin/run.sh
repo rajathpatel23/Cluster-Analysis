4d6eb941081b0be416a694ba77f39e85f91a52ab
Rajat Patel <rpatel@interos.ai>
2021-07-11 17:07:33 -0400

    updated structure

--- bin/run.sh
+++ bin/run.sh
@@ -5,7 +5,7 @@ for Algo in KMeans GMM
   for dim in 150 #10 50 100 200 300
     do
       echo "Running $Algo with dim $dim";
-      python score.py --entities fasttext --clustering_algo $Algo --use_dims $dim
+      python code/score.py --entities fasttext --clustering_algo $Algo --use_dims $dim
   done
 done
 
